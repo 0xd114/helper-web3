c254752942a64408746935689c27e506980340b9
null@null.com <null@null.com>
2022-09-19 20:44:54 +0800

    add more func

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from web3ctf import util
 
-print(util.str_32bytes("abcde"))
+print(util.str_conv("abcde"))
 
 for i in range(0,256):
-    if util.uint8_sha3(i)['hex'] == '0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365':
+    if util.sha3(['uint8'], [i])['hex'] == '0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365':
         print(i)
 
-print(util.get_storage('0x7580DECF0b54F9DfBFcf927a2298f04C8bFB53Df', 1))
+print(util.get_storage('0x7580DECF0b54F9DfBFcf927a2298f04C8bFB53Df', 0))
+print(util.describe_tx('0xa8c17c5ac1b1ec07683814e6daee062cc78a7394fcc91848b484f73e01bb485a'))
+print(util.describe_block(12973093))
+
+print(util.hex_conv(util.sha3(['bytes32', 'uint256'], ['0xaa790a0e56c3d1dcceb22ec33e725624ba465d30b83e78538845c572226ceb3d', 1653079824])['hex']))
--- web3ctf/util.py
+++ web3ctf/util.py
@@ -1,4 +1,6 @@
 from web3 import Web3, HTTPProvider
+from hexbytes import HexBytes
+import typing
 import binascii
 import os
 
@@ -8,34 +10,78 @@ providers = dict()
 providers['ropsten'] = Web3(HTTPProvider(f'https://ropsten.infura.io/v3/{INFURA_API_KEY}'))
 
 
-'''return string to 32 bytes representation'''
-def str_32bytes(st: str) -> str:
+'''return string representations'''
+def str_conv(st: str) -> str:
 
     assert len(st) < 32, "too long :("
 
     ret = Web3.toHex(st.encode())
     ret = ret + '0' * (66-len(ret))
-    return ret
-
-
-'''return sha3 / keccak of int in form of hex and bytes'''
-def uint8_sha3(x: int) -> (str, bytes):
-
-    assert (x < 256 and x >= 0), "0 - 255 only"
-    ret = Web3.keccak(x).hex()
-
     return {
-        'hex': ret, 
-        'bytes': binascii.unhexlify(ret[2:])
+        'hex': HexBytes(ret).hex(),
     }
 
 
-'''return storage at slot_no of a smart contract address'''
+def hex_conv(hx: str) -> typing.Dict[str, typing.Any]:
+    
+    return {
+        'uint256': Web3.toInt(hexstr=hx),
+        'uint8': Web3.toInt(hexstr=hx) % 256
+    }
+
+'''return sha3 / keccak of abi.encodePacked values
+see: https://web3py.readthedocs.io/en/stable/web3.main.html?highlight=keccak#web3.Web3.solidityKeccak'''
+def sha3(types: list[str], values: list[typing.Any]) -> typing.Dict[str, typing.Any]:
+    
+    types2 = []
+    for _type in types:
+        val = _type
+        if val == 'uint':
+            val = 'uint256'
+        types2.append(val)
+    types = types2
+    
+    # types_chk = ['uint256', 'uint8', 'bool', 'address', 'bytes32']
+
+    # for _type in types:
+    #     assert _type in types_chk, "Unsupported types"
+
+    ret = Web3.solidityKeccak(types, values)
+    
+    return {
+        'hex': ret.hex(), 
+        'bytes32': binascii.unhexlify(ret.hex()[2:])
+    }
+
+
+'''return storage at slot_no of a smart contract address representation'''
 def get_storage(addr: str, 
                 slot_no: int, 
-                provider: str = 'ropsten') -> str:
-    res = providers['ropsten'].eth.get_storage_at(addr, slot_no)
+                provider: str = 'ropsten') -> typing.Dict[str, typing.Any]:
+
+    res = providers[provider].eth.get_storage_at(addr, slot_no)
     return {
         'int': Web3.toInt(res),
-        'bytes': res
-    } 
+        'hex': res.hex()
+    }
+
+
+'''describe important transaction properties'''
+def describe_tx(tx_id: str, provider: str = 'ropsten') -> typing.Dict[str, typing.Any]:
+
+    res = providers[provider].eth.getTransaction(tx_id)
+    return {
+        'from': res['from'],
+        'to': res['to'],
+        'block_number': res['blockNumber'],
+    }
+
+'''describe important block properties'''
+def describe_block(blk_id: str, provider: str = 'ropsten') -> typing.Dict[str, typing.Any]:
+
+    res = providers[provider].eth.get_block(blk_id)
+    return {
+        'hash': res['hash'].hex(),
+        'timestamp': res['timestamp']
+    }
+
